9148b2c33283687897f5fd889b9a13ddf70c2275
Francisco E. Zappa <zappafrancisco@gmail.com>
2025-09-06 22:54:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 80;
+const HOST = '0.0.0.0';
 
 // Middleware
 app.use(cors());
@@ -145,6 +146,6 @@ app.get('/api/stats', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}${PORT}`);
 });
