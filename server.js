6041b51829c1017ddcb8137fff7cccf4819e25f3
Francisco E. Zappa <zappafrancisco@gmail.com>
2025-09-07 20:17:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { pool, testConnection, initializeDatabase } = require('./config/database');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
